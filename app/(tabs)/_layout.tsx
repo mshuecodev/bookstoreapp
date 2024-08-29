2063b6b837a48109889e89920e13f88555524829
misna04 <misnasyari@gmail.com>
2024-08-29 21:51:57 +0800

    first config

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,37 +1,81 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Tabs } from "expo-router"
+import React from "react"
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { TabBarIcon } from "@/components/navigation/TabBarIcon"
+import { Colors } from "@/constants/Colors"
+import { useColorScheme } from "@/hooks/useColorScheme"
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+	const colorScheme = useColorScheme()
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
-  );
+	return (
+		<Tabs
+			screenOptions={{
+				tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+				headerShown: false,
+				tabBarShowLabel: false
+			}}
+		>
+			<Tabs.Screen
+				name="index"
+				options={{
+					title: "Home",
+					tabBarIcon: ({ color, focused }) => (
+						<TabBarIcon
+							name="home"
+							color={color}
+						/>
+					)
+				}}
+			/>
+			<Tabs.Screen
+				name="explore"
+				options={{
+					title: "Explore",
+					tabBarIcon: ({ color, focused }) => (
+						<TabBarIcon
+							name="book-open"
+							color={color}
+						/>
+					)
+				}}
+			/>
+			<Tabs.Screen
+				name="author"
+				options={{
+					title: "Author",
+					tabBarIcon: ({ color, focused }) => (
+						<TabBarIcon
+							name="users"
+							color={color}
+						/>
+					)
+				}}
+			/>
+			<Tabs.Screen
+				name="search"
+				options={{
+					title: "Search",
+					tabBarIcon: ({ color, focused }) => (
+						<TabBarIcon
+							name="search"
+							color={color}
+						/>
+					)
+				}}
+			/>
+			<Tabs.Screen
+				name="inbox"
+				options={{
+					title: "Inbox",
+					tabBarIcon: ({ color, focused }) => (
+						<TabBarIcon
+							name="inbox"
+							color={color}
+						/>
+					)
+				}}
+			/>
+		</Tabs>
+	)
 }
---
+++ app/(tabs)/search.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function SearcScreen() {
+	return <div>SearcScreen</div>
+}
