d9001df6b160a593d3040094b9e38e9eb0659a2d
misna04 <misnasyari@gmail.com>
2024-12-02 16:09:58 +0800

    platform condition fixed

--- services/secureStoreService.ts
+++ services/secureStoreService.ts
@@ -13,11 +13,21 @@ export const saveToken = async (token: string) => {
 }
 
 export const getToken = async (): Promise<string | null> => {
-	return await SecureStore.getItemAsync(TOKEN_KEY)
+	if (Platform.OS === "android" || Platform.OS === "ios") {
+		return await SecureStore.getItemAsync(TOKEN_KEY)
+	} else if (Platform.OS === "web") {
+		return localStorage.getItem(TOKEN_KEY)
+	} else {
+		return null
+	}
 }
 
 export const deleteToken = async () => {
-	await SecureStore.deleteItemAsync(TOKEN_KEY)
+	if (Platform.OS === "android" || Platform.OS === "ios") {
+		await SecureStore.deleteItemAsync(TOKEN_KEY)
+	} else if (Platform.OS === "web") {
+		localStorage.removeItem(TOKEN_KEY)
+	}
 }
 
 export const saveRefreshToken = async (refreshToken: string) => {
